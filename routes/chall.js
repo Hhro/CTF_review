01153908e7cb1c788aca2f9fdf89bbcef170e38b
Hhro <kwonsw212@gmail.com>
2019-01-08 17:50:31 +0900

    submit_change

--- routes/chall.js
+++ routes/chall.js
@@ -7,19 +7,26 @@ const router = express.Router();
 router.post('/submit/:id',isLoggedIn,async (req,res,next) => {
     const {id} = req.params;
     const {flag} = req.body;
-
-    await Chall.find({attributes: ['flag'], where: {id: parseInt(id)}})
-    .then((result) => {
-        correct_flag = result.dataValues;
-    })
-    if(correct_flag['flag'] === flag){
-        return res.render('correct',{
-            title: 'CTF_Review',
-            user: req.user,
-        });
-    }
-    else{
-        return res.render('incorrect');
+    try{
+        await Chall.find({attributes: ['flag'], where: {id: parseInt(id)}})
+        .then((result) => {
+            correct_flag = result.dataValues;
+        })
+        if(correct_flag['flag'] === flag){
+            return res.render('chall',{
+                user: req.user,
+                correct: 1,
+            });
+        }
+        else{
+            return res.render('chall',{
+                user: req.user,
+                incorrect: 1,
+            });
+        }
+    } catch(error){
+        console.error(error);
+        return next(error);
     }
 })
 
